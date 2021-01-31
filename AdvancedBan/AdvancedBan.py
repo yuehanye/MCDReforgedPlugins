611aa75b208fd6b26ff2c8dc2608fcdddc4531d9
zhang-anzhi <zhang_anzhi@126.com>
2021-01-31 22:00:57 +0800

    Support MCDR 1.x

--- AdvancedBan/AdvancedBan.py
+++ AdvancedBan/AdvancedBan.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# v0.0.1
-
 import re
 import os
 import json
 import time
 
-FILE_PATH = './plugins/AdvancedBan/'
+from mcdreforged.plugin.server_interface import ServerInterface
+from mcdreforged.api.command import *
+
 PERMISSIONS = {
+    'help': 2,
     'kick': 2,
     'ban': 3,
     'tempban': 3,
@@ -16,7 +17,16 @@ PERMISSIONS = {
 }
 
 adv_data = {}
-HelpMessage = '''
+FILE_PATH = './config/AdvancedBan/'
+PLUGIN_METADATA = {
+    'id': 'advanced_ban',
+    'version': '0.0.2',
+    'name': 'AdvancedBan',
+    'description': 'A player manage plugin',
+    'author': 'zhang_anzhi',
+    'link': 'https://github.com/zhang-anzhi/MCDReforgedPlugins/tree/master/AdvancedBan'
+}
+HELP_MESSAGE = '''
 ------AdvancedBan 玩家管理插件------
 §6!!ab §7显示帮助
 §6!!kick <§6§oplayer§6> [§6§oreason§6] §7踢出玩家
@@ -26,98 +36,132 @@ HelpMessage = '''
 §6!!banlist §7封禁列表'''
 
 
-def init_data():
+def load_data():
+    """Load data from file"""
     global adv_data
-    with open(FILE_PATH + 'AdvancedBan.json') as init:
-        adv_data = json.load(init)
+    with open(FILE_PATH + 'AdvancedBan.json') as f:
+        adv_data = json.load(f)
 
 
-def on_load(server, old):
-    # load data
-    if not os.path.exists(FILE_PATH):
-        os.makedirs(FILE_PATH)
-    if not os.path.isfile(FILE_PATH + 'AdvancedBan.json'):
-        with open(FILE_PATH + 'AdvancedBan.json', 'w') as create:
-            create.write('{}')
-    init_data()
+def save_data():
+    """Save data to file"""
+    global adv_data
+    with open(FILE_PATH + 'AdvancedBan.json', 'w', encoding='utf-8') as save:
+        json.dump(adv_data, save, indent=4, ensure_ascii=False)
 
 
-def on_player_joined(server, player):
-    baned_check(server, player)
-
-
-def on_user_info(server, info):
-    if info.content.rstrip(' ') == '!!ab':
-        return server.reply(info, HelpMessage)
-
-    # kick
-    permission = server.get_permission_level(info)
-    content = info.content.split(' ')
+def register_command(server: ServerInterface):
+    """Register commands"""
+    # ab
+    server.register_command(
+        Literal('!!ab').
+            requires(lambda src: src.has_permission(PERMISSIONS['help']),
+                     lambda: '§c权限不足').
+            runs(lambda src: src.reply(HELP_MESSAGE))
+    )
 
     # kick
-    if info.content.startswith('!!kick '):
-        if len(content) >= 2 and permission >= PERMISSIONS['kick']:
-            player = content[1]
-            reason = f'§c你被踢出游戏! 原因: {content[2]}' if len(content) == 3 else None
-            kick(server, player, reason)
+    server.register_command(
+        Literal('!!kick').
+            requires(lambda src: src.has_permission(PERMISSIONS['kick']),
+                     failure_message_getter=lambda: '§c权限不足').
+            then(
+            Text('player').
+                runs(lambda src, ctx: kick(src.get_server(), ctx['player'])).
+                then(
+                GreedyText('reason').
+                    runs(lambda src, ctx: kick(src.get_server(), ctx['player'],
+                                               ctx['reason']))
+            )
+        )
+    )
 
     # ban
-    elif info.content.startswith('!!ban '):
-        if len(content) >= 2 and permission >= PERMISSIONS['ban']:
-            player = content[1]
-            if player in adv_data.keys():
-                return server.reply(info, '§c该玩家已被封禁')
-            reason = content[2] if len(content) == 3 else None
-            ban_time = int(time.time())
-            adv_data[player] = {
-                'operator': info.player,
-                'ban_time': ban_time,
-                'unban_time': -1,
-                'reason': reason
-            }
-            save_data()
-            baned_check(server, player)
-            server.reply(info, f'§c已封禁{player}')
+    def ban(src, ctx):
+        player = ctx['player']
+        if player in adv_data.keys():
+            return src.reply('§c该玩家已被封禁')
+        adv_data[player] = {
+            'operator': src.player if src.is_player else None,
+            'ban_time': int(time.time()),
+            'unban_time': -1,
+            'reason': ctx.get('reason', None)
+        }
+        save_data()
+        baned_check(src.get_server(), player)
+        src.reply(f'§c已封禁{player}')
 
-    # temp ban
-    elif info.content.startswith('!!tempban '):
-        if len(content) >= 3 and permission >= PERMISSIONS['tempban']:
-            player = content[1]
-            if player in adv_data.keys():
-                return server.reply(info, '§c该玩家已被封禁')
-            reason = content[3] if len(content) == 4 else None
-            ban_time = int(time.time())
-            try:
-                unban_time = ban_time + calc_time(content[2])
-            except:
-                return server.reply(info, '§c时间不合法')
-            adv_data[player] = {
-                'operator': info.player,
-                'ban_time': ban_time,
-                'unban_time': unban_time,
-                'reason': reason
-            }
-            save_data()
-            baned_check(server, player)
-            server.reply(info, f'§c已封禁{player}')
+    server.register_command(
+        Literal('!!ban').
+            requires(lambda src: src.has_permission(PERMISSIONS['ban']),
+                     failure_message_getter=lambda: '§c权限不足').
+            then(
+            Text('player').
+                runs(ban).
+                then(
+                GreedyText('reason').runs(ban)
+            )
+        )
+    )
+
+    # tempban
+    def tempban(src, ctx):
+        player = ctx['player']
+        if player in adv_data.keys():
+            return src.reply('§c该玩家已被封禁')
+        ban_time = int(time.time())
+        try:
+            unban_time = ban_time + calc_time(ctx['time'])
+        except ValueError:
+            return src.reply('§c时间不合法')
+        adv_data[player] = {
+            'operator': src.player if src.is_player else None,
+            'ban_time': ban_time,
+            'unban_time': unban_time,
+            'reason': ctx.get('reason', None)
+        }
+        save_data()
+        baned_check(server, player)
+        src.reply(f'§c已封禁{player}')
+
+    server.register_command(
+        Literal('!!tempban').
+            requires(lambda src: src.has_permission(PERMISSIONS['tempban']),
+                     failure_message_getter=lambda: '§c权限不足').
+            then(
+            Text('player').
+                then(
+                Text('time').
+                    runs(tempban).
+                    then(GreedyText('reason').runs(tempban)
+                )
+            )
+        )
+    )
 
     # unban
-    elif info.content.startswith('!!unban '):
-        if len(content) == 2 and permission >= PERMISSIONS['unban']:
-            player = content[1]
-            if player in adv_data.keys():
-                del adv_data[player]
-                server.reply(info, '§a已解禁' + player)
-                save_data()
-            else:
-                server.reply(info, '§c该玩家未被封禁')
+    def unban(src, ctx):
+        player = ctx['player']
+        if player in adv_data.keys():
+            del adv_data[player]
+            src.reply(f'§a已解禁{player}')
+            save_data()
+        else:
+            src.reply('§c该玩家未被封禁')
+
+    server.register_command(
+        Literal('!!unban').
+            requires(lambda src: src.has_permission(PERMISSIONS['unban']),
+                     failure_message_getter=lambda: '§c权限不足').
+            then(Text('player').runs(unban))
+    )
 
     # banlist
-    elif info.content == '!!banlist' and permission >= PERMISSIONS['banlist']:
+    def banlist(src):
         i = 0
-        server.reply(info, '§8>>> §7Banlist:')
-        server.reply(info, '§cPlayer §8| §e Duration §8| §7Banned by')
-        server.reply(info, '§cType §8> §7Reason')
+        src.reply('§8>>> §7Banlist:')
+        src.reply('§cPlayer §8| §e Duration §8| §7Banned by')
+        src.reply('§cType §8> §7Reason')
         for player, data in adv_data.items():
             i += 1
             if data['unban_time'] == -1:
@@ -126,25 +170,44 @@ def on_user_info(server, info):
             else:
                 duration = f'{int(data["unban_time"] - time.time())}秒'
                 ban_type = 'Tempban'
-            server.reply(info, '')
-            server.reply(
-                info, '§8[§e{}§8]'.format(time.strftime(
-                    "%Y.%m.%d-%H:%M", time.localtime(data['ban_time']))))
-            server.reply(info, f'§c{player} §8| §e{duration} §8| §7{data["operator"]}')
-            server.reply(info, f'§c{ban_type} §8> §7{data["reason"]}')
+            src.reply('')
+            src.reply('§8[§e{}§8]'.format(
+                time.strftime(
+                    '%Y.%m.%d-%H:%M', time.localtime(data['ban_time'])
+                )
+            ))
+            src.reply(f'§c{player} §8| §e{duration} §8| §7{data["operator"]}')
+            src.reply(f'§c{ban_type} §8> §7{data["reason"]}')
+
+    server.register_command(
+        Literal('!!banlist').
+            requires(lambda src: src.has_permission(PERMISSIONS['banlist']),
+                     failure_message_getter=lambda: '§c权限不足').
+            runs(banlist)
+    )
 
 
-def kick(server, player, reason):
-    if reason is None:
-        reason = ''
+def on_load(server: ServerInterface, old):
+    # load data
+    if not os.path.exists(FILE_PATH):
+        os.makedirs(FILE_PATH)
+    if not os.path.isfile(FILE_PATH + 'AdvancedBan.json'):
+        save_data()
+    load_data()
+    server.register_help_message('!!ab', 'AdvancedBan帮助信息',
+                                 permission=min(PERMISSIONS.values()))
+    register_command(server)
+
+
+def on_player_joined(server, player, info):
+    baned_check(server, player)
+
+
+def kick(server, player, reason=None):
+    reason = '' if reason is None else f' 原因: {reason}'
+    reason = f'§c你被踢出游戏!' + reason
+    server.logger.info(reason)
     server.execute(' '.join(['kick', player, reason]).rstrip(' '))
-
-
-def save_data():
-    global adv_data
-    with open(FILE_PATH + 'AdvancedBan.json', 'w', encoding='utf-8') as save:
-        json.dump(adv_data, save, indent=4, ensure_ascii=False)
-        save.flush()
 
 
 def calc_time(s):
@@ -182,15 +245,19 @@ def baned_check(server, player):
     global adv_data
     if player in adv_data.keys():
         reason = adv_data[player]['reason']
+        server.logger.info(reason)
         if adv_data[player]['unban_time'] == -1:
-            reason = f'§c永久封禁原因: {reason}'
-            return kick(server, player, reason)
-        elif adv_data[player]['unban_time'] > time.time():
-            duration = adv_data[player]['unban_time'] - time.time()
-            reason = '§c封禁原因: {} §c解禁时间: {}'.format(reason, time.strftime(
-                '%Y-%m-%d %H:%M:%S', time.localtime(
-                    adv_data[player]['unban_time'])))
+            reason = f'永久封禁{"" if reason is None else f"（{reason}）"}'
             kick(server, player, reason)
-        if adv_data[player]['unban_time'] < time.time():
+        elif adv_data[player]['unban_time'] > time.time():
+            reason = '§c{}, 解禁时间: {}'.format(
+                reason,
+                time.strftime(
+                    '%Y-%m-%d %H:%M:%S',
+                    time.localtime(adv_data[player]['unban_time'])
+                )
+            )
+            kick(server, player, reason)
+        elif adv_data[player]['unban_time'] < time.time():
             del adv_data[player]
             save_data()
